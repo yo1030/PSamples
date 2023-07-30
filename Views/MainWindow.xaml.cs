ff34500c7b565465c9720ccadef7e2438a7b5274
yo1030 <0vant1c@gmail.com>
2023-07-30 09:40:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PSamples.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
