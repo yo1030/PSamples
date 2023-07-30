ffa64232e29b71f5f45d0af2e09b29f16bc1c6ec
yo1030 <0vant1c@gmail.com>
2023-07-30 10:04:54 +0900

    label と button のデータバインディング

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,11 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace PSamples.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Prism Application Sample";
         public string Title
         {
             get { return _title; }
@@ -13,7 +14,23 @@ namespace PSamples.ViewModels
 
         public MainWindowViewModel()
         {
+            SystemDateUpdateBtn = new DelegateCommand(SystemDateUpdateBtnExecute);
+        }
 
+        //SystemDateLabel
+        private string _systemDateLabel = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        public string SystemDateLabel
+        {
+            get { return _systemDateLabel; }
+            set { SetProperty(ref _systemDateLabel, value); }
+        }
+
+        //SystemDateUpdateBtn
+        public DelegateCommand SystemDateUpdateBtn { get; }
+
+        private void SystemDateUpdateBtnExecute()
+        {
+            SystemDateLabel = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
         }
     }
 }
