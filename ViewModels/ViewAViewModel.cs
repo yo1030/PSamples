cbb7dcaf0627ef2f1867fa6f080861ebd16e7348
yo1030 <0vant1c@gmail.com>
2023-07-31 20:08:32 +0900

    popup風の画面表示

--- ViewModels/ViewAViewModel.cs
+++ ViewModels/ViewAViewModel.cs
@@ -1,24 +1,39 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using Prism.Services.Dialogs;
+using PSamples.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 
 namespace PSamples.ViewModels
 {
     public class ViewAViewModel : BindableBase, INavigationAware
     {
         private string _myLabel = String.Empty;
+        private IDialogService _dialogService;
         public string MyLabel
         {
             get { return _myLabel; }
             set { SetProperty(ref _myLabel, value); }
         }
 
-        public ViewAViewModel()
+        public ViewAViewModel(IDialogService dialogService)
         {
+            _dialogService = dialogService;
+            OKButton = new DelegateCommand(OKButtonExecute);
+        }
 
+        public DelegateCommand OKButton { get; }
+
+        private void OKButtonExecute ()
+        {
+            //MessageBox.Show("Save");
+            var p = new DialogParameters();
+            p.Add(nameof(ViewBViewModel.ViewBTextBox), "Save");
+            _dialogService.ShowDialog(nameof(ViewB), p, null);
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
