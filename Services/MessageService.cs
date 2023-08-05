cd6e22cef2c6de6fb63b9310733ea2412378559e
yo1030 <0vant1c@gmail.com>
2023-08-05 20:16:19 +0900

    メッセージボックス表示

---
+++ Services/MessageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PSamples.Services
+{
+    public sealed class MessageService : IMessageService
+    {
+        public MessageBoxResult Question(string message)
+        {
+            return MessageBox.Show(
+                message,
+                "問い合わせ",
+                MessageBoxButton.OKCancel,
+                MessageBoxImage.Question);
+        }
+
+        public void ShowDialog(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
--- ViewModels/ViewAViewModel.cs
+++ ViewModels/ViewAViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Mvvm;
 using Prism.Regions;
 using Prism.Services.Dialogs;
+using PSamples.Services;
 using PSamples.Views;
 using System;
 using System.Collections.Generic;
@@ -14,19 +15,28 @@ namespace PSamples.ViewModels
     {
         private string _myLabel = String.Empty;
         private IDialogService _dialogService;
+        private IMessageService _messageService;
         public string MyLabel
         {
             get { return _myLabel; }
             set { SetProperty(ref _myLabel, value); }
         }
 
-        public ViewAViewModel(IDialogService dialogService)
+        public ViewAViewModel(IDialogService dialogService) : this(dialogService, new MessageService())
+        {
+
+        }
+
+        public ViewAViewModel(IDialogService dialogService, IMessageService messageService)
         {
             _dialogService = dialogService;
+            _messageService = messageService;
             OKButton = new DelegateCommand(OKButtonExecute);
+            OKButton2 = new DelegateCommand(OKButton2Execute);
         }
 
         public DelegateCommand OKButton { get; }
+        public DelegateCommand OKButton2 { get; }
 
         private void OKButtonExecute ()
         {
@@ -34,6 +44,14 @@ namespace PSamples.ViewModels
             var p = new DialogParameters();
             p.Add(nameof(ViewBViewModel.ViewBTextBox), "Save");
             _dialogService.ShowDialog(nameof(ViewB), p, null);
+        }
+        private void OKButton2Execute()
+        {
+            //MessageBox.Show("Save");
+            if (_messageService.Question("Saveしますか？") == MessageBoxResult.OK)
+            {
+                _messageService.ShowDialog("Saveしました");
+            }
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
